de4039f92878318ad22dc94dc649a8f14b491c33
RafaelD3v <rafaelcontato.ce@gmail.com>
2022-02-25 12:26:14 -0300

    Corrected page name

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -58,7 +58,7 @@ export function Upload() {
 
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de arquivos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
